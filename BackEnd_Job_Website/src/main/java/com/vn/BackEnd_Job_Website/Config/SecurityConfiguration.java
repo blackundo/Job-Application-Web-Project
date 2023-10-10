73598fbb2502c7c478bd8297fee5440956a79fb6
blackundo <blackundovn@gmail.com>
2023-10-11 05:11:01 +0700

    add cors filter

--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Config/SecurityConfiguration.java
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Config/SecurityConfiguration.java
@@ -21,8 +21,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf()
-                .disable()
+                .cors()
+                .and()
+                .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/**")
                 .permitAll()
--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Controller/auth/AuthenticationController.java
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Controller/auth/AuthenticationController.java
@@ -3,10 +3,7 @@ package com.vn.BackEnd_Job_Website.Controller.auth;
 import com.vn.BackEnd_Job_Website.Service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
