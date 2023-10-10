e0db2e84769b214a36c354a574934dc104d1266a
blackundo <blackundovn@gmail.com>
2023-10-10 23:18:45 +0700

    create rest api reg and login

---
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Controller/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.vn.BackEnd_Job_Website.Controller.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
