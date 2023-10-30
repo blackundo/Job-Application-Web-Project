bebf9a6d8dfdae79baf48513223540beec48a06a
blackundo <blackundovn@gmail.com>
2023-10-30 18:03:17 +0700

    add new datasource

--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.vn.BackEnd_Job_Website.Model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Service/FreelancerService.java
+++
@@ -1,8 +0,0 @@
-package com.vn.BackEnd_Job_Website.Service;
-
-import com.vn.BackEnd_Job_Website.Model.Freelancer;
-
-public interface FreelancerService {
-    Freelancer getFreelancerByEmail(String email);
-    Freelancer updateFreelancerProfile(Freelancer updatedFreelancer);
-}
