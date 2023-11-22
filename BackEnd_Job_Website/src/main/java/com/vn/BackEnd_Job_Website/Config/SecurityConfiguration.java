507c356de0faa1ba2f05d3c47fed940e13d00c84
blackundo <blackundovn@gmail.com>
2023-11-23 03:41:08 +0700

    update profile

--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Config/SecurityConfiguration.java
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Config/SecurityConfiguration.java
@@ -4,16 +4,13 @@ import com.vn.BackEnd_Job_Website.Exception.CustomAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
