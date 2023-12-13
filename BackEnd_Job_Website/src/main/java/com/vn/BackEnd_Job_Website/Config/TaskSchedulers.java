a093babd8a00017a67b27af60cb08f8959597cb0
Đỗ Phước Đạt <85673985+dophuocdat@users.noreply.github.com>
2023-12-14 01:56:57 +0700

    Scheduler Status Jobs

---
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Config/TaskSchedulers.java
@@ -0,0 +1,16 @@
+package com.vn.BackEnd_Job_Website.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+
+public class TaskSchedulers {
+
+    @Bean
+    public TaskScheduler TaskScheduler(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(2);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+}
