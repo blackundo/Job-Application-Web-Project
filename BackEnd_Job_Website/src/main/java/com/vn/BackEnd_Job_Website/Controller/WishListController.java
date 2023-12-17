2e328ce08e3b177610a168d54cf44bc9654236db
blackundo <blackundovn@gmail.com>
2023-12-17 23:08:33 +0700

    update wishlist

--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Controller/WishListController.java
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Controller/WishListController.java
@@ -1,5 +1,6 @@
 package com.vn.BackEnd_Job_Website.Controller;
 
+import com.amazonaws.services.kms.model.NotFoundException;
 import com.vn.BackEnd_Job_Website.Model.*;
 import com.vn.BackEnd_Job_Website.Respository.CandidateRepository;
 import com.vn.BackEnd_Job_Website.Respository.HiringRepository;
@@ -11,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -23,23 +25,47 @@ public class WishListController {
 
     @GetMapping("/")
     public ResponseEntity<?> get_wish(){
-        Account account = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Candidate candidate = candidateRepository.findByAccountID(account.getId()).orElseThrow();
-        List<WishList> list = wishListRepository.findByCandidateID_Id(candidate.getId());
-        return new ResponseEntity<>(list, HttpStatus.OK);
-    }
-    @PostMapping("/{id}")
-    public void wished(@RequestPart Integer hiring_id){
-        Account account = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Candidate candidate = candidateRepository.findByAccountID(account.getId()).orElseThrow();
-        var hiring = hiringRepository.findById(hiring_id).orElseThrow();
-
-        WishList wishList = new WishList(null, candidate, hiring);
-        wishListRepository.save(wishList);
+        try {
+            Account account = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Candidate candidate = candidateRepository.findByAccountID(account.getId())
+                    .orElseThrow(() -> new NotFoundException("Candidate not found")); // Tạo exception NotFound khi không tìm thấy Candidate
+            List<WishList> list = wishListRepository.findByCandidateID_Id(candidate.getId());
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error fetching wish list: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @DeleteMapping("/")
-    public void wishes(@RequestBody Hiring hiring){
+    /**
+     * @param requestBody hiring_id
+     * @return
+     */
+    @PostMapping("/add")
+    public ResponseEntity<?> addWish(@RequestBody Map<String, Integer> requestBody) {
+        try {
+            Integer hiringId = requestBody.get("hiring_id");
+            if (hiringId == null) {
+                return new ResponseEntity<>("Hiring ID is required", HttpStatus.BAD_REQUEST);
+            }
+
+            Account account = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Candidate candidate = candidateRepository.findByAccountID(account.getId())
+                    .orElseThrow(() -> new NotFoundException("Candidate not found"));
+
+            var hiring = hiringRepository.findById(hiringId)
+                    .orElseThrow(() -> new NotFoundException("Hiring not found"));
+
+            WishList wishList = new WishList(null, candidate, hiring);
+            wishListRepository.save(wishList);
+
+            return new ResponseEntity<>("Added to wishlist successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error adding to wishlist: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void wishes(@RequestPart Hiring hiring){
         WishList wishList = wishListRepository.findByHiringID(hiring.getId()).orElseThrow();
         wishListRepository.delete(wishList);
     }
