71132b88c2d5a70dc1d5c93df26248af7090d8b2
blackundo <blackundovn@gmail.com>
2023-12-19 18:16:56 +0700

    config vite

--- frontend_job_website/vite.config.js
+++ frontend_job_website/vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
     },
     // End workaround
   ],
+  build: {chunkSizeWarningLimit: 4500}
   // Workaround before renaming .js to .jsx
   optimizeDeps: {
     esbuildOptions: {
