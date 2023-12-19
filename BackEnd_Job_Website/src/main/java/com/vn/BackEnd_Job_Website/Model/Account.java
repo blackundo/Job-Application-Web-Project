edf0d9fc415656e2bf324a091d2522923a038fa3
blackundo <blackundovn@gmail.com>
2023-12-19 07:15:19 +0700

    fix modal false

--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Model/Account.java
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Model/Account.java
@@ -40,7 +40,7 @@ public class Account implements UserDetails {
     private String password;
 
     @Column(name = "Status")
-    private Boolean status = false;
+    private Boolean status = Boolean.FALSE;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
